95c0fbe3a916e4ae88ee1bb270368d4619a21929
MggZ2011 <105609214+MggZ2011@users.noreply.github.com>
2025-07-22 14:22:16 -0400

    Beta-1.0

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@
 // For example, using VITE_SUPABASE_URL in a .env.local file.
 export const SUPABASE_URL = "https://flajemusminumtluybwr.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsYWplbXVzbWludW10bHV5YndyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MDY3OTcsImV4cCI6MjA2NzQ4Mjc5N30.Rzzu-DEl-s2bE4tHpCVg6NYe_yJYE7Xivw6one7XecE";
-export const GOOGLE_MAPS_API_KEY = "AIzaSyCfl6VMlwIpaiUmJBhLBswUbHt10YS0IC0";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyAJ1fClUCzDfwQlAswnnx-486JrUw80rmg";
